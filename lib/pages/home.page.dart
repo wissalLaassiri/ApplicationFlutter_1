01b2c4ea42b6aa807a0743cb9b711cf948a18374
wissalLaassiri <wissal.laassiri@gmail.com>
2021-04-02 11:49:00 +0100

    app_1

---
+++ lib/pages/home.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_first_app/widgets/drawer.widget.dart';
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MyDrawerWidget(),
+      appBar: AppBar(title:Text("Home"),),
+      body: Center(
+        child: Text("Home"),
+      ),
+
+    );
+  }
+}
